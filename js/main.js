876e41dfb515fddcc8de06db8dbda98f54dc8668
Maribel-17 <maribel.plaza17@gmail.com>
2024-11-12 12:46:38 -0300

    subo archivos

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector("#menu");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const menuItems = document.querySelectorAll("#menu .lista-menu a"); 
+
+abrir.addEventListener("click", () => {
+  menu.classList.add("visible"); 
+});
+
+cerrar.addEventListener("click", () => { 
+  menu.classList.remove("visible"); 
+});
+
+
+menuItems.forEach(item => {
+  item.addEventListener("click", () => {
+    menu.classList.remove("visible"); 
+  });
+});
+
